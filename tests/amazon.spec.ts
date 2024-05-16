5e048b608ec8310c912efa3379cfeef23fc52296
byuno <robert.y.uno@gmail.com>
2024-05-16 09:14:07 -0700

    newPage attempt

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -1,8 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('Go to Amazon', async ({ page }) => {
-    await page.goto('https://www.amazon.ca/');
+// test('Exercise #1 - Click on Watchlist while logged out', async ({ page }) => {
+//     await page.goto('https://www.amazon.ca/');
   
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle('Amazon.ca: Low Prices – Fast Shipping – Millions of Items');
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('Amazon.ca: Low Prices – Fast Shipping – Millions of Items');
+//   });
+
+  test.only('Exercise #2 - Change the currency while logged out', async ({ page, context }) => {
+    await page.goto('https://www.amazon.ca/');
+    
+    await page.getByLabel('Choose a language for shopping.').first().hover()
+    
+    await page.getByText('Change country/region.').first().click()
+
+    await expect(page.getByText("Website (Country/Region)")).toBeVisible()
+
+    await page.getByText('Canada').nth(2).click();
+    
+    await page.getByRole("listbox").getByText('Japan (日本)').click()
+    
+    const pagePromise = context.waitForEvent('page');
+    
+    //Use dispatched events to overcome 'intercepts pointer event' issue I was getting. 
+    await page.locator('#icp-save-button #icp-save-button-announce').getByText(' Go to website ').dispatchEvent('click');
+    
+    const newPage = await pagePromise;
+    
+    await newPage.getByLabel('Choose a language for shopping.').first().hover();
+
+    //await page.locator('#nav-tools #icp-nav-flyout').waitFor(); 
+
+    //await page.locator('#nav-tools #icp-nav-flyout').click();
   });
