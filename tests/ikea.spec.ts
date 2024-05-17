7fffbceeaa927cf2de4d7f49d243dfdc272e95fb
byuno <robert.y.uno@gmail.com>
2024-05-17 15:31:44 -0700

    navigate to Ikea

--- tests/ikea.spec.ts
+++ tests/ikea.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('Exercise #3 - Add Ikea furniture to shopping list', async ({ page }) => {
+    await page.goto('https://www.ikea.com/ca/en/');
     
 })
