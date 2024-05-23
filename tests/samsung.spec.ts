f1e3255c9f9d3803df29ea273b98d92b6ca96975
byuno <robert.y.uno@gmail.com>
2024-05-23 12:02:19 -0700

    hover over mobile button

--- tests/samsung.spec.ts
+++ tests/samsung.spec.ts
@@ -1,5 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test('Exercise #5 - Verify Samsung.com exclusive colours', async({page}) => {
+    
+    //Go to Samsung.com
     await page.goto('http://www.samsung.com/');
+
+    //Click Accept All on the cookies modal
+    const acceptAllBtn = await page.locator('.truste-consent-content #truste-consent-buttons').getByRole('button', {name: 'Accept All'});
+    await acceptAllBtn.click();
+
+    //Click on Mobile category
+    const mobileMenuBtn = await page.getByRole('menubar').getByRole('listitem').getByRole('menuitem', {name: 'Mobile'})
+    await mobileMenuBtn.hover();
+    
 })
