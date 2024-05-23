25174f497d95cec6e9492de43d14d5b40afd4640
byuno <robert.y.uno@gmail.com>
2024-05-23 10:36:34 -0700

    goto Samsung.com

--- tests/samsung.spec.ts
+++ tests/samsung.spec.ts
@@ -1,1 +1,5 @@
 import { test, expect } from '@playwright/test';
+
+test('Exercise #5 - Verify Samsung.com exclusive colours', async({page}) => {
+    await page.goto('http://www.samsung.com/');
+})
